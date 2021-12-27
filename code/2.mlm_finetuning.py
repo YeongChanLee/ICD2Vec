9aaf6c618af013353e6a674aed118a07fe1b3ed3
YeongChanLee <conan_8th@naver.com>
2021-12-27 15:38:44 +0900

    Add files via upload

--- code/2.mlm_finetuning.py
+++ code/2.mlm_finetuning.py
@@ -1,46 +1,10 @@
-from transformers import BertTokenizer, BertForMaskedLM, TrainingArguments, Trainer, FeatureExtractionPipeline
-from transformers import AutoTokenizer, AutoModelForMaskedLM, AutoConfig, pipeline
+from transformers import TrainingArguments, Trainer, AutoTokenizer, AutoModelForMaskedLM, AutoConfig
 
 import torch
-import csv
 import numpy as np
+from utils import get_dataset
 
-data_encoding = dict()  # For storing ICD codes and its short description
-flag = 0
-
-with open("../data/ukb_coding19.tsv") as fd:  # Loading the Csv file containing ICD codes and its short description
-    rd = csv.reader(fd, delimiter="\t", quotechar='"')
-    for row in rd:  # iterating through each row
-        if (flag == 0):  # Ignoring first row
-            flag = 1
-            continue
-        a = row[1].split(' ', 1)[1]
-        data_encoding[row[0]] = a  # Storing description in a dict
-
-app_syn = dict()  # For storing approximate synonyms corresponding to each ICD code
-cli_info = dict()  # For storing clinical information corresponding to each ICD code
-
-with open("../data/icd_info4.csv", 'r',
-          encoding='utf-8') as fd:  # "icd_info4.csv" files contains clinical information and aprroximate synonyms of ICD codes
-    rd = csv.reader(fd, delimiter=",", quotechar='"')
-    for row in rd:
-        row[0] = row[0].split('-')[0]
-        try:
-            row[0] = row[0].split('.')[0] + row[0].split('.')[1]
-        except:
-            row[0] = row[0].split('.')[0]
-        cli_info[row[0]] = row[3]
-        app_syn[row[0]] = row[4]
-
-data_encoding_comp = dict()    #for storing complete representation of ICD Codes
-for key, value in data_encoding.items():
-    data_encoding_comp[key] = value
-    if key in cli_info.keys():
-        if len(cli_info[key]) > 0:  #Adding clinical information to ICD code description if present
-            data_encoding_comp[key] = data_encoding_comp[key] + " " + cli_info[key]
-    if key in app_syn.keys():
-        if len(app_syn[key]) > 0:   #Adding approximate synonyms to ICD codes description if present
-            data_encoding_comp[key] = data_encoding_comp[key] + " " + app_syn[key]
+data_encoding_comp = get_dataset()
 
 text = list(data_encoding_comp.values())
 text = np.repeat(text, 5)
@@ -71,7 +35,7 @@ for i in range(inputs.input_ids.shape[0]):
     inputs.input_ids[i, selection[i]] = 103
 
 
-class MeditationsDataset(torch.utils.data.Dataset):
+class CreateDataset(torch.utils.data.Dataset):
     def __init__(self, encodings):
         self.encodings = encodings
 
@@ -81,7 +45,7 @@ class MeditationsDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.encodings.input_ids)
 
-dataset = MeditationsDataset(inputs)
+dataset = CreateDataset(inputs)
 
 args = TrainingArguments(
     output_dir='out',
